4323ee697ae2cd3172b2ea9a78fd9355c79102c5
kuiper90 <dobritoiu.ana@gmail.com>
2019-06-14 14:01:55 +0300

    Postfix Polish Notation calculator with unit tests

---
+++ Project_PostFixPolishNotation/Project_PostFixPolishNotation/PostFixPolishNotation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Project_PostFixPolishNotation
+{
+    public static class PostFixPolishNotation
+    {
+        public static decimal EvaluateExpression(string expression)
+        {
+            string[] expr = expression.Split(' ');
+            int exprLen = expr.Count();
+
+            if (exprLen > 2)
+                return EvaluateExpr(expr);
+            throw new Exception("Expression doesn't contain enough elments.");
+        }
+
+        private static decimal EvaluateExpr(string[] expression)
+        {
+            decimal number;
+            Stack<decimal> rez = expression
+            .Aggregate(new Stack<decimal>(), (acc, elem) =>
+            {
+
+                if (decimal.TryParse(elem, out number))
+                {
+                    acc.Push(number);
+                }
+                else
+                {
+                    switch (elem)
+                    {
+                        case "*":
+                            {
+                                acc.Push(acc.Pop() * acc.Pop());
+                                break;
+                            }
+                        case "/":
+                            {
+                                number = acc.Pop();
+                                acc.Push(acc.Pop() / number);
+                                break;
+                            }
+                        case "+":
+                            {
+                                acc.Push(acc.Pop() + acc.Pop());
+                                break;
+                            }
+                        case "-":
+                            {
+                                number = acc.Pop();
+                                acc.Push(acc.Pop() - number);
+                                break;
+                            }
+                        default:
+                            throw new Exception("Invalid expression.");
+                    }
+                }
+                return acc;
+            });
+            return rez.Pop();
+        }
+    }
+}
---
+++ UnitTest_PostFixPolishNotation/UnitTest_PostFixPolishNotation.cs
@@ -0,0 +1,91 @@
+using System;
+using Xunit;
+using static Project_PostFixPolishNotation.PostFixPolishNotation;
+
+namespace UnitTest_PostFixPolishNotation
+{
+    public class UnitTest_PostFixPolishNotation
+    {
+        [Fact]
+        public void ValidPostFixExpr1_ShouldReturn_Value()
+        {
+            string expression = "9 5 4 * - 2 +";
+            Assert.True(EvaluateExpression(expression) == -9);
+        }
+
+        [Fact]
+        public void ValidPostFixExpr2_ShouldReturn_Value()
+        {
+            string expression = "9 5 - 2 7 + *";
+            Assert.True(EvaluateExpression(expression) == 36);
+        }
+
+        [Fact]
+        public void ValidPostFixExpr3_ShouldReturn_Value()
+        {
+            string expression = "9 5 * 2 7 * +";
+            Assert.True(EvaluateExpression(expression) == 59);
+        }
+
+        [Fact]
+        public void ValidPostFixExpr4_ShouldReturn_Value()
+        {
+            string expression = "9 5 + 2 + 7 +";
+            Assert.True(EvaluateExpression(expression) == 23);
+        }
+
+        [Fact]
+        public void InvalidPostFixExpr1_ShouldReturn_Value()
+        {
+            string expression = "9 +";
+            var exception = Record.Exception(() => EvaluateExpression(expression));
+            Assert.IsType(typeof(Exception), exception);
+            Assert.True(exception.Message == "Expression doesn't contain enough elments.");
+        }
+
+        [Fact]
+        public void InvalidPostFixExpr2_ShouldReturn_Error()
+        {
+            string expression = "9 7";
+            var exception = Record.Exception(() => EvaluateExpression(expression));
+            Assert.IsType(typeof(Exception), exception);
+            Assert.True(exception.Message == "Expression doesn't contain enough elments.");
+        }
+
+        [Fact]
+        public void InvalidPostFixExpr3_ShouldReturn_Error()
+        {
+            string expression = "9";
+            var exception = Record.Exception(() => EvaluateExpression(expression));
+            Assert.IsType(typeof(Exception), exception);
+            Assert.True(exception.Message == "Expression doesn't contain enough elments.");
+        }
+
+        [Fact]
+        public void InvalidPostFixExpr4_ShouldReturn_Error()
+        {
+            string expression = "+";
+            var exception = Record.Exception(() => EvaluateExpression(expression));
+            Assert.IsType(typeof(Exception), exception);
+            Assert.True(exception.Message == "Expression doesn't contain enough elments.");
+        }
+
+        [Fact]
+        public void InvalidPostFixExpr5_ShouldReturn_Error()
+        {
+            string expression = "";
+            var exception = Record.Exception(() => EvaluateExpression(expression));
+            Assert.IsType(typeof(Exception), exception);
+            Assert.True(exception.Message == "Expression doesn't contain enough elments.");
+        }
+
+        [Fact]
+        public void InvalidPostFixExpr6_ShouldReturn_Error()
+        {
+            string expression = "1 2 b 4 a";
+            var exception = Record.Exception(() => EvaluateExpression(expression));
+            Assert.IsType(typeof(Exception), exception);
+            Assert.True(exception.Message == "Invalid expression.");
+        }
+    }
+}
