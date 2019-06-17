5bd96ec3700c3ea9491fe0151201985d2618b4d3
kuiper90 <dobritoiu.ana@gmail.com>
2019-06-17 14:54:47 +0300

    review changes: added exception method, replaced List with Enumerable

--- Project_PostFixPolishNotation/Project_PostFixPolishNotation/PostFixPolishNotation.cs
+++ Project_PostFixPolishNotation/Project_PostFixPolishNotation/PostFixPolishNotation.cs
@@ -6,51 +6,55 @@ namespace Project_PostFixPolishNotation
 {
     public static class PostFixPolishNotation
     {
-        static readonly string[] ops = { "+", "-", "*", "/" };
-
         public static decimal EvaluateExpression(string expression)
         {
             string[] expr = expression.Split(' ');
             
-            if (expr.Count() > 2)
-                return EvalExpr(expr);
-            throw new Exception("Expression doesn't contain enough elements.");
+            if (expr.Count() < 3)
+                throw new Exception("Expression doesn't contain enough elements.");
+            return EvalExpr(expr);
         }
 
         static decimal EvalExpr(string[] expression)
         {
-            List<decimal> result = expression
-                .Aggregate(new List<decimal>(), (coll, item) => {
-                    if (Array.IndexOf(ops, item) == -1)
-                    {
-                        if (decimal.TryParse(item, out decimal number) == false)
-                            throw new Exception("Invalid expression.");
-                        coll.Add(number);
-                    }
-                    else
-                    {
-                        if (coll.Count() < 2)
-                            throw new Exception("Invalid expression.");
-                        decimal nrOne = coll[coll.Count - 2];
-                        decimal nrTwo = coll[coll.Count - 1];                        
-                        coll = coll
-                            .Take(coll.Count - 2).ToList();
-                        coll.Add(ApplyOperator(item, nrOne, nrTwo));
-                    }
-                    return coll;
-                });
-            return result[0];
+            IEnumerable<decimal> result = expression
+            .Aggregate(Enumerable.Empty<decimal>(), (coll, item) =>
+            {
+                if (decimal.TryParse(item, out decimal number))
+                    coll = coll.Append(number);
+                else
+                {
+                    if (coll.Count() < 2)
+                        InvalidExpressionException();
+                    decimal nrOne = coll.ElementAt(coll.Count() - 2);
+                    decimal nrTwo = coll.ElementAt(coll.Count() - 1);
+                    coll = coll
+                        .Take(coll.Count() - 2).ToList();
+                    coll = coll.Append(ApplyOperator(item, nrOne, nrTwo));
+                }
+                return coll;
+            });
+            return result.First();
         }
 
         private static decimal ApplyOperator(string op, decimal nrOne, decimal nrTwo)
         {
+            string[] ops = { "+", "-", "*", "/" };
+
             Func<decimal, decimal, decimal>[] opList = new Func<decimal, decimal, decimal>[] { (a, b) => a + b,
                                                                                                (a, b) => a - b,
                                                                                                (a, b) => a * b,
                                                                                                (a, b) => a / b  };
             int opIndex = Array.IndexOf(ops, op);
 
+            if (opIndex == -1)
+                InvalidExpressionException();
             return opList[opIndex](nrOne, nrTwo);
+        }
+
+        private static void InvalidExpressionException()
+        {
+            throw new Exception("Invalid expression.");
         }
     }
 }
